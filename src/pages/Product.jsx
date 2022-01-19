80b0cf975e6080d3bdad90aede095f3a0fa5983c
Denis <sldenys96@hotmail.com>
2022-01-19 07:53:25 +0100

    product filters added

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,3 +1,4 @@
+import { Remove, Add } from '@material-ui/icons';
 import Announcement from '../components/Announcement';
 import Navbar from '../components/Navbar';
 import Newsletter from '../components/Newsletter';
@@ -15,6 +16,32 @@ const Product = () => {
                         <h1 className="theProd__title">Cool T-Shirt</h1>
                         <p className="theProd__desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                         <span className="theProd__price">20$</span>
+                        <div className="theProd__filterCont">
+                            <div className="theProd__filter">
+                                <span className="theProd__filterTitle"></span>
+                                <div className="theProd__filterColor black"></div>
+                                <div className="theProd__filterColor blue"></div>
+                                <div className="theProd__filterColor gray"></div>
+                            </div>
+                            <div className="theProd__filter">
+                                <span className="theProd__filterTitle">Size</span>
+                                <select name="" id="" className="theProd__filterSize">
+                                    <option value="xs" className="theProd__filterSizeOption">xs</option>
+                                    <option value="s" className="theProd__filterSizeOption">s</option>
+                                    <option value="m" className="theProd__filterSizeOption">m</option>
+                                    <option value="l" className="theProd__filterSizeOption">l</option>
+                                    <option value="xl" className="theProd__filterSizeOption">xl</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div className="theProd__addCont">
+                            <div className="theProd__amountCont">
+                                <Remove/>
+                                <span className="theProd__amount">1</span>
+                                <Add />
+                            </div>
+                            <button className='theProd__addBtn'>ADD TO CART</button>
+                        </div>
                     </div>
                 </div>
                 <Newsletter />
